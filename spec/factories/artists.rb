8c307a30cda7b61e4b93c5e9b8415d6a2a81094c
Ruben Marin <rubenmarin@gmail.com>
2020-01-26 21:41:55 -0500

    rspec example

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :artist do
     name          { Faker::Name.name }
     image         { Faker::Internet.url }
-    popularity    { Faker::Number.number }
     spotify_url   { Faker::Code.asin }
   end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+artist = FactoryGirl.create(:artist)
+
 RSpec.describe Artist, type: :model do
   it "has a valid factory" do
     expect(artist).to be_valid
